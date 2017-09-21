7dae70f3682232559e7d92eef3ea30ac81235b35
ambooger <amberlburroughs@gmail.com>
2017-09-21 00:14:55 -0700

    up date user input variable

--- hangman.js
+++ hangman.js
@@ -1,15 +1,15 @@
 
 // filter function - Amber 
 document.addEventListener("keyup", function(event){
-    var userinput =  event.key.toLowerCase();// key press value
+    var userInput =  event.key.toLowerCase();// key press value
     patt=/[A-Za-z]/g; // pattern to check against - g is global
-    var isLetter = patt.test(userinput); // check if input is a letter T/F
+    var isLetter = patt.test(userInput); // check if input is a letter T/F
     console.log(isLetter); 
   
     if (isLetter) {
         // true
-        console.log(userinput);
-        // functionName(userinput); pass any function that needs the key press value here.
+        console.log(userInput);
+        // functionName(userInput); pass any function that needs the key press value here.
         // pass userinput in as the an argument.
     } else { 
         // false
